b6468e44987f73824faf54c0cfafca91f140a84d
ewing.rj@gmail.com <ewing.rj@gmail.com@2c9f4be6-b864-9f10-fb66-ed5b87761d4d>
2014-02-15 00:53:42 +0000

    check if user is an admin for any expedtions and add to session

---
+++ trunk/src/auth/authorizer.java
@@ -0,0 +1,40 @@
+package auth;
+
+import bcid.database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by rjewing on 2/7/14.
+ */
+public class authorizer {
+    protected Connection conn;
+    private database db;
+
+    public authorizer() throws Exception{
+        db = new database();
+        conn = db.getConn();
+    }
+
+    public Boolean userExpeditionAdmin(String username) {
+        PreparedStatement stmt;
+        try {
+            Integer users_id = db.getUserId(username);
+            String selectString = "SELECT count(*) as count FROM expeditions WHERE users_id = ?";
+
+            stmt = conn.prepareStatement(selectString);
+            stmt.setInt(1, users_id);
+
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+            return rs.getInt("count") >= 1;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
