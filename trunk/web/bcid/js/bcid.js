804b7bd5fc1beeafc570dcfb4dfe176de1e180e0
ewing.rj@gmail.com <ewing.rj@gmail.com@2c9f4be6-b864-9f10-fb66-ed5b87761d4d>
2014-03-11 20:07:07 +0000

    user profile page now loads profile editor via ajax call

--- trunk/web/bcid/js/bcid.js
+++ trunk/web/bcid/js/bcid.js
@@ -180,3 +180,14 @@ function updateProjectConfig(select) {
             $('#projectValidationXML').text(data[0]['validation_xml']);
         });
 }
+
+function getQueryParam(sParam) {
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) {
+            return sParameterName[1];
+        }
+    }
+}
