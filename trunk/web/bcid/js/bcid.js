1b6fff6fee8ca52999814ec088c5cdfa24c17b21
ewing.rj@gmail.com <ewing.rj@gmail.com@2c9f4be6-b864-9f10-fb66-ed5b87761d4d>
2014-03-18 22:03:39 +0000

    get rid of window.location.reload, instead use the populateBCIDPage function

--- trunk/web/bcid/js/bcid.js
+++ trunk/web/bcid/js/bcid.js
@@ -12,7 +12,7 @@ function dataGroupEditorSubmit() {
             if (data[0].error != null) {
                 $(".error").html(data[0].error);
             } else {
-                window.location.reload();
+                populateBCIDPage();
             }
         });
 }
@@ -472,4 +472,33 @@ function populateResourcesOrDatasets(divId) {
             divId,
             'Unable to load this expedition\'s datasets from server.');
     }
+}
+
+function populateBCIDPage() {
+    populateDivFromService(
+        "/id/groupService/listUserBCIDsAsTable",
+        "listUserBCIDsAsTable",
+        "Unable to load this user's BCIDs from Server");
+    $(document).one("ajaxStop", function() {
+        $("a.edit").click(function() {
+            populateDivFromService(
+                "/id/groupService/dataGroupEditorAsTable?ark=" + this.dataset.ark,
+                "listUserBCIDsAsTable",
+                "Unable to load the BCID editor from Server");
+        });
+        $(document).one("ajaxStop", function() {
+            populateSelect("resourceTypesMinusDataset");
+            $("#cancelButton").click(function() {
+                populateBCIDPage();
+            });
+            $(document).one("ajaxStop", function() {
+                var options = $('option')
+                $.each(options, function() {
+                    if ($('select').data('resource_type') == this.text) {
+                        $('select').val(this.value);
+                    }
+                });
+            });
+        });
+    });
 }
