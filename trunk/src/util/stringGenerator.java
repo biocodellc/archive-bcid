13634694e41d1f59ab43cd771a41bb362eedd6b8
ewing.rj@gmail.com <ewing.rj@gmail.com@2c9f4be6-b864-9f10-fb66-ed5b87761d4d>
2014-05-10 22:34:39 +0000

    changed a character in the stringGenerator

--- trunk/src/util/stringGenerator.java
+++ trunk/src/util/stringGenerator.java
@@ -17,7 +17,7 @@ public class stringGenerator {
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'M', 'N',
             'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
             '2', '3', '4', '5', '6', '7', '8', '9',
-            '-', '!'
+            '-', '_'
     };
 
     // Generate a random string of a specific length
