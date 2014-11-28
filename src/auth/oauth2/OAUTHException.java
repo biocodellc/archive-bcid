08c5f7b7a924350bcc2b010edfa97d47879a2d97
ewing.rj@gmail.com <ewing.rj@gmail.com@2c9f4be6-b864-9f10-fb66-ed5b87761d4d>
2014-11-28 23:15:19 +0000

    oauth2.provider.java better uses exceptions

---
+++ src/auth/oauth2/OAUTHException.java
@@ -0,0 +1,11 @@
+package auth.oauth2;
+
+/**
+ * An exception that encapsulates errors from the bcid system.
+ */
+public class OAUTHException extends Exception {
+    public OAUTHException() { super(); }
+    public OAUTHException(String message) { super(message); }
+    public OAUTHException(String message, Throwable cause) { super(message, cause); }
+    public OAUTHException(Throwable cause) { super(cause); }
+}
