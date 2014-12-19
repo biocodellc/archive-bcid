b80fbbbb9b413b852466e3fc5e9cb1fdc4b1164d
ewing.rj@gmail.com <ewing.rj@gmail.com@2c9f4be6-b864-9f10-fb66-ed5b87761d4d>
2014-12-19 05:00:46 +0000

    accidentially modified interface without modifying implementing classes

--- src/bcid/encoder.java
+++ src/bcid/encoder.java
@@ -1,5 +1,7 @@
 package bcid;
 
+import bcidExceptions.BCIDException;
+
 import java.math.BigInteger;
 
 /**
@@ -9,5 +11,5 @@ public interface encoder {
     static char[] chars = null;
     static  byte[] codes = new byte[256];
     public String encode(BigInteger identifier);
-    public BigInteger decode(String identifier);
+    public BigInteger decode(String identifier) throws BCIDException;
 }
