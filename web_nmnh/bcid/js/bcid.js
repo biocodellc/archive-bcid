8e542d7ee89b5b946877ea86b92dc3a289edf815
John Deck <jdeck88@gmail.com>
2015-04-29 11:13:57 -0700

    changed Entrust Login to LDAP login so i can continue developing in master... 2-factor testing should be forked

--- web_nmnh/bcid/js/bcid.js
+++ web_nmnh/bcid/js/bcid.js
@@ -717,7 +717,8 @@ function loadingDialog(promise) {
 
 // function to login user
 function login() {
-    var url = "/id/authenticationService/loginEntrust";
+    var url = "/id/authenticationService/loginLDAP";
+    //var url = "/id/authenticationService/loginEntrust";
     var return_to = getQueryParam("return_to");
     if (return_to != null) {
         url += "?return_to=" + return_to;
